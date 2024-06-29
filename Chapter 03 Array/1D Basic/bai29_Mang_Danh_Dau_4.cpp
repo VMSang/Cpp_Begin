fa433be6c1539e6bece92f40cfb9b56384d31526
VMSang <vosang20102005@gmail.com>
2024-06-29 10:37:26 +0700

    Cleanup: Improve file naming conventions

--- Chapter 03 Array/1D Basic/bai29_Mang_Danh_Dau_4.cpp
+++ Chapter 03 Array/1D Basic/bai29_Mang_Danh_Dau_4.cpp
@@ -2,31 +2,31 @@
 using namespace std;
 
 int cnt[1000001];
-int main(){
-    int n; cin>>n;
+int main() {
+    int n; cin >> n;
     int a[n];
     int maxvalue = INT_MIN;
-    for(int i = 0; i<n; i++){
-        cin>>a[i];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
         maxvalue =  max(maxvalue , a[i]);
     }
-    for(int i=0; i<n; i++){
-        cnt[a[i]] +=1;
+    for (int i = 0; i < n; i++) {
+        cnt[a[i]] += 1;
     }
     int max_ts = 0, pos_max;
-    for(int i=0; i<=maxvalue; i++){
-       if(cnt[i] > max_ts){
+    for (int i = 0; i <= maxvalue; i++) {
+        if (cnt[i] > max_ts) {
             max_ts = cnt[i];
-            pos_max= i;
-       }
+            pos_max = i;
+        }
     }
     //neu yeu cau: lấy ra gia tri dau tien nếu có nhiều gtri trùng lần xuat hien
-    for(int x : a){
-        if (cnt[x] > max_ts){
+    for (int x : a) {
+        if (cnt[x] > max_ts) {
             max_ts = cnt[x];
             pos_max = x;
         }
     }
-    cout<<pos_max<<" "<<max_ts;
+    cout << pos_max << " " << max_ts;
     return 0;
 }
