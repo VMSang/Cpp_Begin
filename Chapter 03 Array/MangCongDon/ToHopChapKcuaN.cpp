849f58d8668b2e275beadfb8deb6deaac3385232
VMSang <vosang20102005@gmail.com>
2024-07-23 20:52:35 +0700

    prefix sum

---
+++ Chapter 03 Array/MangCongDon/ToHopChapKcuaN.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[10000][10000];
+void init(){
+	for(int i=0; i<=10000; i++){
+		for(int j=0; j<=i; j++){
+			
+		}
+	}
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);    
+    int ts; cin>>ts;
+    while(ts--){
+        
+    }
+    return 0;
+}
